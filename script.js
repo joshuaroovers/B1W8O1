8ce7ce84af0788cf27761300aadcf098ac89e8a8
Joshua <Doerak1aug@gmail.com>
2019-10-15 13:41:32 +0200

    perfection!

--- script.js
+++ script.js
@@ -2,63 +2,146 @@ prijstotaal = 0;
 bestel()
 
 function bestel(){
-    welke = prompt("Welke bestelling wilt u toevoegen?\n(fris, bier, wijn)\n(\"stop\" voor de rekening)")    
+
+    welke = prompt("Welke bestelling wilt u toevoegen?\n(fris, bier, wijn, snack)\n(\"stop\" voor de rekening)")    
     welke = welke.toLowerCase();
-    if(welke == "stop"){
-        
-        stop()
+
+    if(welke == "stop")
+    {        
+        rekening()
     }
-    else if(welke == "fris"){
-        
+    else if(welke == "fris")
+    {        
         keuze = "fris";
         prijsper = 1.90;
         hoeveelheidbestel()
     }
-    else if(welke == "bier"){
-        
+    else if(welke == "bier")
+    {
         keuze = "bier";
         prijsper = 3.25;
         hoeveelheidbestel()
     }
-    else if(welke == "wijn"){
-        
+    else if(welke == "wijn")
+    {
         keuze = "wijn";
         prijsper = 4.10;
         hoeveelheidbestel()
     }
-    else{
-        
-        alert("Invalide input");
+    else if(welke == "snack")
+    {
+        keuze = "bitterballen";
+        hoeveelheidB()
+    }
+    else
+    {
+        alert("U heeft een ongeldige invoer gedaan.");
         bestel()
     }
 }
 
-function hoeveelheidbestel(){
+function hoeveelheidbestel()
+{   
     aantal = prompt("Hoeveel " + keuze + " wilt u toevoegen aan uw bestelling?\n(voer \"a\" in om te annuleren")
+    
     if(aantal == "a" || aantal == "A")
     {
         alert("Geanuleerd")
         bestel()
     }
-    else if(isNaN(aantal)){
+    else if(isNaN(aantal))
+    {
         alert("U dient een getal in te voeren")
         bestel()
     }
-    else if(aantal < 1){
+    else if(aantal < 1)
+    {
         alert("minimum van 1 vereist")
         bestel()
     }
-    else{
-        prijssubtotaal = prijsper*aantal;
-        prijssubtotaal = prijssubtotaal.toFixed(2);
-        document.write(aantal + " " + keuze + " € " + prijssubtotaal + "<br>") 
-        prijssubtotaal = parseFloat(prijssubtotaal);
-        prijstotaal = prijstotaal + prijssubtotaal;
-        bestel()
+    else
+    {
+        calc()
     }
 }
+function calc()
+{
+        prijssubtotaal = prijsper*aantal;
 
-function stop(){
+        prijssubtotaal = prijssubtotaal.toFixed(2);
+
+        document.write(aantal + " " + keuze + " € " + prijssubtotaal + "<br>") 
+        
+        prijssubtotaal = parseFloat(prijssubtotaal);
+
+        prijstotaal = prijstotaal + prijssubtotaal;
+        bestel()
+}
+function hoeveelheidB()
+{
+    aantalB = prompt("Hoeveel " + keuze + " wilt u toevoegen aan uw bestelling (8 of 16)?\n(voer \"a\" in om te annuleren")
+        
+        
+    if(aantalB == 8)
+    {
+        prijsper = 6
+        aantal = prompt("Hoeveel bitterbalschalen van " + aantalB + " stuks wilt u bestellen?")
+        if (aantal < 1)
+        {
+             alert("minimum van 1 vereist")
+             bestel()
+        }
+        else if(isNaN(aantal))
+        {
+            alert("U dient een getal in te voeren")
+            bestel()
+        }
+        else
+        {
+            keuze = "bitterbalschaal 8"
+            calc()
+        }
+    }
+    else if(aantalB == 16)
+    {
+        prijsper = 10.5
+        aantal = prompt("Hoeveel bitterbalschalen van " + aantalB + " stuks wilt u bestellen?")
+        if (aantal < 1)
+        {
+            alert("minimum van 1 vereist")
+            bestel()
+        }
+        else if(isNaN(aantal))
+        {
+            alert("U dient een getal in te voeren")
+            bestel()
+        }
+        else
+        {
+            keuze = "bitterbalschaal 16"
+            calc()
+        }
+    }
+    else if(aantalB == "a" || aantalB == "A")
+    {
+        alert("Geanuleerd")
+        bestel()
+    }
+    else if(isNaN(aantalB))
+    {
+        alert("U dient een getal in te voeren")
+        bestel()
+    }
+    else if(aantalB != 8 || aantalB != 16 )
+    {
+        alert("U kunt alleen een keuze maken tussen 8 en 16.")
+        bestel()
+    }
+        
+}
+
+function rekening()
+{
     prijstotaal = prijstotaal.toFixed(2);
     document.write("<br>" + "Totaal:\n" + "€ " + prijstotaal)
 }
