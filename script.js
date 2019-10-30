1d8b05a126c5cef66467ba667863dec4377b3c16
Joshua <Doerak1aug@gmail.com>
2019-10-30 14:26:58 +0100

    .

--- script.js
+++ script.js
@@ -1,4 +1,26 @@
+const prijsfris = 1.90;
+const prijsbier = 3.25;
+const prijswijn = 4.10;
+
+const prijsB8 = 6;
+const prijsB16 = 10.5;
+
+totaalbedragfris = 0;
+totaalbedragbier = 0;
+totaalbedragwijn = 0;
+
+totaalbedragB8 = 0;
+totaalbedragB16 = 0;
+
+var totaalfris = 0;
+var totaalbier = 0;
+var totaalwijn = 0;
+
+var totaalB8 = 0;
+var totaalB16 = 0;
+
 prijstotaal = 0;
+
 bestel()
 
 function bestel(){
@@ -13,19 +35,16 @@ function bestel(){
     else if(welke == "fris")
     {        
         keuze = "fris";
-        prijsper = 1.90;
         hoeveelheidbestel()
     }
     else if(welke == "bier")
     {
         keuze = "bier";
-        prijsper = 3.25;
         hoeveelheidbestel()
     }
     else if(welke == "wijn")
     {
         keuze = "wijn";
-        prijsper = 4.10;
         hoeveelheidbestel()
     }
     else if(welke == "snack")
@@ -66,17 +85,39 @@ function hoeveelheidbestel()
 }
 function calc()
 {
-        prijssubtotaal = prijsper*aantal;
+    if(keuze == "fris")
+    {
+        totaalfris === totaalfris + aantal;
+        totaalbedragfris = totaalbedragfris + aantal*prijsfris
+        bestel();
+    }
+    else if(keuze == "bier")
+    {
+        totaalbier = totaalbier + aantal;
+        totaalbedragbier = totaalbedragbier + aantal*prijsbier
+        bestel();
+    }
+    else if(keuze == "wijn")
+    {
+        totaalwijn = totaalwijn + aantal;
+        totaalbedragwijn = totaalbedragwijn + aantal*prijswijn
+        bestel();
+    }
+    else if(keuze == "bitterbalschaal 8")
+    {
+        totaalB8 = totaalB8 + aantal;
+        totaalbedragB8 = totaalbedragB8 + aantal*prijsB8
+        bestel();
+    }
+    else if(keuze == "bitterbalschaal 16")
+    {
+        totaalB16 = totaalB16 + aantal;
+        totaalbedragB16 = totaalbedragB16 + aantal*prijsB16
+        bestel();
+    }
 
-        prijssubtotaal = prijssubtotaal.toFixed(2);
-
-        document.write(aantal + " " + keuze + " € " + prijssubtotaal + "<br>") 
-        
-        prijssubtotaal = parseFloat(prijssubtotaal);
-
-        prijstotaal = prijstotaal + prijssubtotaal;
-        bestel()
 }
+
 function hoeveelheidB()
 {
     aantalB = prompt("Hoeveel " + keuze + " wilt u toevoegen aan uw bestelling (8 of 16)?\n(voer \"a\" in om te annuleren")
@@ -84,7 +125,7 @@ function hoeveelheidB()
         
     if(aantalB == 8)
     {
-        prijsper = 6
+        prijsper = prijsB8
         aantal = prompt("Hoeveel bitterbalschalen van " + aantalB + " stuks wilt u bestellen?")
         if (aantal < 1)
         {
@@ -104,7 +145,7 @@ function hoeveelheidB()
     }
     else if(aantalB == 16)
     {
-        prijsper = 10.5
+        prijsper = prijsB16
         aantal = prompt("Hoeveel bitterbalschalen van " + aantalB + " stuks wilt u bestellen?")
         if (aantal < 1)
         {
@@ -142,6 +183,18 @@ function hoeveelheidB()
 
 function rekening()
 {
+    totaalbedragfris = totaalbedragfris.toFixed(2);
+    totaalbedragbier = totaalbedragbier.toFixed(2);
+    totaalbedragwijn = totaalbedragwijn.toFixed(2);
+    totaalbedragB8 = totaalbedragB8.toFixed(2);
+    totaalbedragB16 = totaalbedragB16.toFixed(2);
+
+    document.write(totaalfris + "fris" + " € " + totaalbedragfris + "<br>")
+    document.write(totaalbier + "bier" +   " € " + totaalbedragbier + "<br>")
+    document.write(totaalwijn + "wijn" +  " € " + totaalbedragwijn + "<br>")
+    document.write(totaalB8 + "bitterbalschaal 8" + " € " + totaalbedragB8 + "<br>")
+    document.write(totaalB16 + "bitterbalschaal 16" + " € " + totaalbedragB16 + "<br>")
+    prijstotaal = totaalbedragfris + totaalbedragbier + totaalbedragwijn + totaalbedragB8 + totaalbedragB16;
     prijstotaal = prijstotaal.toFixed(2);
     document.write("<br>" + "Totaal:\n" + "€ " + prijstotaal)
 }
